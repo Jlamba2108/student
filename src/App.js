e4308dd4647187ce2d19f3308a57e08ad50b0f95
Jlamba2108 <80780450+Jlamba2108@users.noreply.github.com>
2022-11-29 23:18:46 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import {studentProfile, studentImage,studentContact} from './components/studentContainer'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <studentProfile />
+        <studentImage />
+        <studentContact />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
